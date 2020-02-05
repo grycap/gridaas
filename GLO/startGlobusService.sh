e6557513628f22b4a5d3adcc6253525b256df77a
J. Damian Segrelles Quilis <dquilis@dsic.upv.es>
2020-02-05 12:16:28 +0100

    Update startGlobusService.sh

--- GLO/startGlobusService.sh
+++ GLO/startGlobusService.sh
@@ -4,4 +4,4 @@
 grid-proxy-init -valid 8760:0 -cert /etc/grid-security/certificates/usercert.crt -key /etc/grid-security/certificates/userkey.pem
 
 # Initialize globus-gatekeeper service
-/etc/init.d/globus-gatekeeper start
+/etc/init.d/globus-gatekeeper start 
